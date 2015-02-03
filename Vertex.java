6bb7e50f0cdee484e53e94b352553c9097c3ddc8
aptaunk <aptaunk@gmail.com>
2015-02-03 16:19:56 -0700

    Update Vertex.java
    I made the vertices comparable because that will come in handy for ordering vertices in edges and triangles
    I also made it so that vertices with the same x and y value will be considered equal and return the same hashcode. I'm not sure if that is strictly necessary but oh well.

--- Vertex.java
+++ Vertex.java
@@ -1,15 +1,33 @@
 public class Vertex implements Comparable<Vertex> {
-  public double x;
-  public double y;
-  
-  public Vertex(double x, double y) {
-    this.x = x;
-    this.y = y;
-  }
-  
-  @Override
-  public int compareTo(Vertex v) {
-    return
-      this.x > v.x ? 1 : (this.x < v.x ? -1 : (this.y > v.y ? 1 : (this.y < v.y ? -1 : 0)));
-  }
+    public double x;
+    public double y;
+    
+    public Vertex(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    @Override
+    public int compareTo(Vertex v) {
+        return this.x > v.x ? 1 : (this.x < v.x ? -1 : (this.y > v.y ? 1 : (this.y < v.y ? -1 : 0)));
+    }
+    
+    @Override
+    public String toString() {
+        return "("+this.x+","+this.y+")";
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (o == this) return true;
+        if (!(o instanceof Vertex)) return false;
+        Vertex v = (Vertex) o;
+        return v.x == this.x && v.y == this.y;
+    }
+    
+    @Override
+    public int hashCode() {
+        return this.toString().hashCode();
+    }
 }
