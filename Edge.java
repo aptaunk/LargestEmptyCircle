7917e64368c95dddaf3bd1ff25b34d9204f0164c
aptaunk <aptaunk@gmail.com>
2015-02-03 16:34:01 -0700

    Create Edge.java
    Edge contains two vertices. For any two vertices v1 and v2, new Edge(v1,v2).equals(new Edge(v2,v1)) should return true. I.e the ordering shouldn't matter because the vectors get sorted in the constructor. The same is true for the hash function. I could make the fields private to formalize this but I'm too lazy.

---
+++ Edge.java
@@ -0,0 +1,27 @@
+public class Edge
+{
+    public Vertex[] v;
+    
+    public Edge(Vertex v1, Vertex v2) {
+        v = new Vertex[2];
+        if (v1.compareTo(v2) < 0) {
+            v[0] = v1;
+        } else {
+            v[1] = v2;
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return "["+v[0].toString()+v[1].toString()+"]";
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (o == this) return true;
+        if (!(o instanceof Vertex)) return false;
+        Edge e = (Edge) o;
+        return e.v[0].equals(this.v[0]) && e.v[1].equals(this.v[1]);
+    }
+}
