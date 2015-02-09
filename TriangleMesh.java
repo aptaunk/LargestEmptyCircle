dc0b927c4bff90c8565ed7198536fa53720b8889
aptaunk <aptaunk@gmail.com>
2015-02-09 12:49:21 -0700

    Update TriangleMesh.java
    TraingleMesh now has a list of triangles and knowledge of the neighbors of each triangle

--- TriangleMesh.java
+++ TriangleMesh.java
@@ -1,8 +1,29 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap ;
 
 public class TriangleMesh
 {
-    public HashSet<Vertex> vertices;
-    public HashSet<Edge> edges;
-    public HashSet<Triangle> triangles;
+    public ArrayList<Triangle> triangles;
+    public HashMap<Edge,Triangle[]> edge2TrianglesMap;
+    
+    public TriangleMesh() {
+        triangles = new ArrayList<Triangle>();
+        edge2TrianglesMap = new HashMap<Edge,Triangle[]>();
+    }
+    
+    public void add(Triangle t) {
+        triangles.add(t);
+        for (Edge e : t.e) {
+            Triangle[] temp = edge2TrianglesMap.get(e);
+            if(temp == null) {
+                edge2TrianglesMap.put(e,new Triangle[]{t,null});
+            } else if (!t.equals(temp[0]) && !t.equals(temp[1])) {
+                if (temp[0]==null) {
+                    temp[0] = t;
+                } else {
+                    temp[1] = t;
+                }
+            }
+        }
+    }
 }
