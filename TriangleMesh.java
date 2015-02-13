955bb2e7e41d4d7843c1063994719624cc92db4e
aptaunk <aptaunk@gmail.com>
2015-02-13 14:43:36 -0700

    Revert triangle mesh
    accidentally updated this as Delauney triangulation

--- TriangleMesh.java
+++ TriangleMesh.java
@@ -1,46 +1,61 @@
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
 
-public class DelaunayTriangulation
+public class TriangleMesh
 {
-    public static TriangleMesh bowyerWatson(Triangle superTriangle, ArrayList<Vertex> s) {
-        TriangleMesh triangulation = new TriangleMesh();
-        triangulation.add(superTriangle);
-        for (Vertex v : s) {
-            ArrayList<Triangle> badTriangles = new ArrayList<Triangle>();
-            for (Triangle t : triangulation.triangles) {
-                if (t.insideCircumcircle(v)) {
-                    badTriangles.add(t);
-                }
-            }
-            ArrayList<Edge> polygon = new ArrayList<Edge>();
-            for (Triangle t : badTriangles) {
-                for (Edge e : t.e) {
-                    Triangle[] temp = triangulation.edge2TrianglesMap.get(e);
-                    if (!triangulation.isSharedEdge(e)) {
-                        polygon.add(e);
-                    }
-                }
-            }
-            for (Triangle t : badTriangles) {
-                triangulation.remove(t);
-            }
-            for (Edge e : polygon) {
-                Triangle newTri = new Triangle(v,e.v[0],e.v[1]);
-                triangulation.add(newTri);
-            }
-        }
-        for (Triangle t : triangulation.triangles) {
-            if (shareVertices(t,superTriangle)) {
-                triangulation.remove(t);
-            }
-        }
-        return triangulation;
+    public HashSet<Triangle> triangles;
+    public HashMap<Edge,Triangle[]> edge2TrianglesMap;
+    
+    public TriangleMesh() {
+        triangles = new HashSet<Triangle>();
+        edge2TrianglesMap = new HashMap<Edge,Triangle[]>();
     }
     
-    private static boolean shareVertices(Triangle t1, Triangle t2) {
-        return 
-            t1.v[0].equals(t2.v[0]) || t1.v[0].equals(t2.v[1]) || t1.v[0].equals(t2.v[2]) ||
-            t1.v[1].equals(t2.v[0]) || t1.v[1].equals(t2.v[1]) || t1.v[1].equals(t2.v[2]) ||
-            t1.v[2].equals(t2.v[0]) || t1.v[2].equals(t2.v[1]) || t1.v[2].equals(t2.v[2]);
+    public void add(Triangle t) {
+        triangles.add(t);
+        for (Edge e : t.e) {
+            Triangle[] temp = edge2TrianglesMap.get(e);
+            if(temp == null) {
+                edge2TrianglesMap.put(e,new Triangle[]{t,null});
+            } else if (!t.equals(temp[0]) && !t.equals(temp[1])) {
+                if (temp[0]==null) {
+                    temp[0] = t;
+                } else {
+                    temp[1] = t;
+                }
+            }
+        }
+    }
+    
+    public void remove(Triangle t) {
+        for (Edge e : t.e) {
+            Triangle[] temp = edge2TrianglesMap.get(e);
+            if (temp!=null) {
+                if (t.equals(temp[0])) {
+                    temp[0] = null;
+                }
+                if (t.equals(temp[1])) {
+                    temp[1] = null;
+                }
+                if (temp[0]==null && temp[1]==null) {
+                    edge2TrianglesMap.remove(e);
+                }
+            }
+        }
+        triangles.remove(t);
+    }
+    
+    public boolean isSharedEdge(Edge e) {
+        Triangle[] temp = edge2TrianglesMap.get(e);
+        return temp[0]!=null && temp[1]!=null;
+    }
+    
+    public Triangle[] getNeighbours(Triangle t) {
+        Triangle[] neighbour = new Triangle[3];
+        for (int i=0; i<3; i++) {
+            Triangle[] temp = edge2TrianglesMap.get(t.e[i]);
+            neighbour[i] = temp[0].equals(t)?temp[1]:temp[0];
+        }
+        return neighbour;
     }
 }
